7b0f115e5f9c6e46871c02c809bb24e296c835b7
Lini <lina.camargo@kitsmile.com>
2015-09-16 21:37:27 -0500

    documentarios ifs

--- bloques.rb
+++ bloques.rb
@@ -1,19 +1,33 @@
 
 #Cree una comparación de nombre
 # se deben usar dos (==) para que compare si se usa solo un (=) asigna unvalor 
+puts "ejecutando linea 4"
 if "lini" == "lini"
 puts "lini es igual a lini"
 end
 
 #rescatando el valor que comparamos
+puts "ejecutando linea 10"
 if "lini" == "andrea"
 	puts "lini es igual a lini"
 else
 	puts "lini no es igua a lini"
 end
-
+puts "ejecutando linea 17"
 if 7 == 2 + 5
 	puts  "el resultado es 7"
 else
 	puts "el resultado no es 7"
 end
+
+
+#Bloque con 2 condiciones 
+puts "ejecutando linea 25"
+if "hola" == "chao"
+	puts "hola es igual a chao"
+elsif "chao" == "chao"
+	puts "chao es igual a chao "
+else 
+	puts "no se cumpli ninguna condición"
+end
+	
