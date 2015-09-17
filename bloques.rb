8a5d7ca5dd58b5d4d46365d6147533df66d446be
Lini <lina.camargo@kitsmile.com>
2015-09-16 19:59:58 -0500

    comparación de valores y retorno de mensajes

---
+++ bloques.rb
@@ -0,0 +1,19 @@
+
+#Cree una comparación de nombre
+# se deben usar dos (==) para que compare si se usa solo un (=) asigna unvalor 
+if "lini" == "lini"
+puts "lini es igual a lini"
+end
+
+#rescatando el valor que comparamos
+if "lini" == "andrea"
+	puts "lini es igual a lini"
+else
+	puts "lini no es igua a lini"
+end
+
+if 7 == 2 + 5
+	puts  "el resultado es 7"
+else
+	puts "el resultado no es 7"
+end
