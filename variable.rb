949fad4064fe1ffb33b990a313fd14077bf8a5cb
Lini <lina.camargo@kitsmile.com>
2015-09-16 19:17:09 -0500

    cree variables

---
+++ variable.rb
@@ -0,0 +1,27 @@
+#Defini una variable de cadena de texto
+nombre = "lina María"
+#Defini una variable de cadena de texto
+Apellido = "Camargo Puerto"
+#defini una variable de numero
+Edad = 24
+variable = "Primera Variable"
+miVariable = 1.67
+booleanoVar = true
+sumaSencilla = 2 + 5
+varYVar = miVariable + sumaSencilla
+texto = "Final"
+#subi los datos a la terminal
+
+=begin 
+Logre el primer coment en bloque. 
+=end
+
+puts texto
+puts nombre
+puts Apellido
+puts Edad
+puts variable
+pust miVariable
+puts booleanoVar
+pust sumaSencilla
+puts varYVar
