17780e9fce72455350c6f0e125aeb7f957fd59c8
Lini <lina.camargo@kitsmile.com>
2015-09-16 20:00:55 -0500

    preguntar nombre y apellido y dar respuesta al concatenar

---
+++ gets.rb
@@ -0,0 +1,8 @@
+# pido que me de su primer nombre
+print "ingrese su nombre"
+Nombre = gets.chomp 
+# pido que me de su primer apellido
+print "ingrese su apellido"	
+Nombre2 = gets.chomp
+#imprimo en la terminal un texto de bienvenida 
+puts "Hola" + Nombre + "" + Nombre2 + "."
