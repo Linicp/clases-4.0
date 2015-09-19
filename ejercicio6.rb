7da7fc7e538ab00232892cb717878b411ad8024e
Lini <lina.camargo@kitsmile.com>
2015-09-18 19:30:42 -0500

    ingrese su nombr y apellido con gets

---
+++ ejercicio6.rb
@@ -0,0 +1,12 @@
+#usando gets pide el nombre el apellido del usuario y 
+#luego imprime los datos en la terminal 
+
+# pido que me de su primer nombre
+print "ingrese su nombre"
+Nombre = gets.chomp 
+# pido que me de su primer apellido
+print "ingrese su apellido"	
+Nombre2 = gets.chomp
+#imprimo en la terminal un texto de bienvenida 
+puts "Hola" + Nombre + "" + Nombre2 + "."
+	
---
+++ metodo.rb
@@ -0,0 +1,19 @@
+def saludar
+	puts "Hola,Bienvenido"
+if "lina" == "lina"
+
+	puts "lina es igual a lina"
+
+else
+	puts "lina no es igual a lina"
+end
+
+end
+
+saludar
+
+def hablar(texto)
+puts "El método dice #{texto}"
+end
+
+hablar ("Estoy hablando con un método que recibe argumentos")
